811a55eaaa0bed2d09a2e88e1aa025e77d7767ce
pol.thomas <pol.thomas@niji.fr>
2021-08-27 17:59:29 +0200

    Portfolio section WIP

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Selection of HTML objects
 const burger = document.querySelector('.burger i');
 const menu = document.querySelector('.menu');
 
-// Defining a function
-function toggleNav() {
+const toggleNav = () => {
     burger.classList.toggle('fa-bars');
     burger.classList.toggle('fa-times');
     menu.classList.toggle('menu--active');
 }
 
-// Calling the function after click event occurs
-burger.addEventListener('click', () => {
-    toggleNav();
-});
+const openPdf = (pdf) => {
+    window.open(pdf);
+}
