ed7127dfde82847a6a97aafdb6f63337b32d52e3
pol.thomas <pol.thomas@niji.fr>
2021-08-31 20:39:33 +0200

    Lot of small fixes and change the logo

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const burger = document.querySelector('.burger i');
-// const burgerClose = document.querySelector('.fa-times');
 const menu = document.querySelector('.menu');
 const navbar = document.querySelector('.navbar-container');
 
@@ -13,12 +12,19 @@ const openPdf = (pdf) => {
     window.open(pdf);
 }
 
-const goToAnchor = (anchor) => {
+const closeMobileMenu = () => {
     menu.classList.remove('menu--active');
-    burger.classList.toggle('fa-bars');
-    burger.classList.toggle('fa-times');
+}
 
+const goToAnchor = (anchor) => {
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
     const loc = document.location.toString().split('#')[0];
+
+    if (windowWidth < 576) {
+        burger.classList.toggle('fa-bars');
+        burger.classList.toggle('fa-times');
+    }
+    closeMobileMenu()
     document.location = loc + '#' + anchor;
 }
 
@@ -26,8 +32,7 @@ window.onscroll = () => {
     "use strict";
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 20 ) {
         navbar.classList.add('navbar-container--on-scroll');
-    }
-    else {
+    } else {
         navbar.classList.remove('navbar-container--on-scroll');
     }
 };
